b653ef6e7e6799883e1e9dc4a900acc61f8b9409
feraranas <46a12sFAASfaas>
2023-11-22 14:01:13 -0600

    added session state

--- app.py
+++ app.py
@@ -179,12 +179,52 @@ if 'Credit_History_Age' not in st.session_state:
 if 'Monthly_Balance' not in st.session_state:
     st.session_state['Monthly_Balance'] = 0
 
+def update_age():
+    st.session_state.age = st.session_state._age
+
+def update_income():
+    st.session_state.income = st.session_state._income
+    
+def update_month():
+        if st.session_state._month == 'January':
+            st.session_state.month = '1'
+        elif st.session_state._month == 'February':
+            st.session_state.month = '2'
+        elif st.session_state._month == 'March':
+            st.session_state.month = '3'
+        elif st.session_state._month == 'April':
+            st.session_state.month = '4'
+        elif st.session_state._month == 'May':
+            st.session_state.month = '5'
+        elif st.session_state._month == 'June':
+            st.session_state.month = '6'
+        elif st.session_state._month == 'July':
+            st.session_state.month = '7'
+        elif st.session_state._month == 'August':
+            st.session_state.month = '8'
+        elif st.session_state._month == 'September':
+            st.session_state.month = '9'
+        elif st.session_state._month == 'October':
+            st.session_state.month = '10'
+        elif st.session_state._month == 'November':
+            st.session_state.month = '11'
+        elif st.session_state._month == 'December':
+            st.session_state.month = '12'  
+
+def update_credit_mix():
+        if st.session_state._credit_mix == 'Good':
+          st.session_state.credit_mix = '0'
+        elif st.session_state._credit_mix == 'Standard':
+          st.session_state.credit_mix = '1'
+        elif st.session_state._credit_mix == 'Bad':
+          st.session_state.credit_mix = '2'  
+
 def set_page_view(page):
     st.session_state['current_step'] = 1 
 #     st.session_state['queued_file'] = None
 #     st.session_state['current_view'] = page      
 
-def set_form_step(parameter_key,parameter_value,action,step=None):
+def set_form_step(action,step=None):
     if action == 'Next':
         st.session_state['current_step'] = st.session_state['current_step'] + 1
     if action == 'Back':
@@ -236,111 +276,83 @@ def wizard_form_header():
     step_cols3[3].button('Credit History Age',on_click=set_form_step,args=['Jump',11],type=credit_history_type)      
     step_cols3[4].button('Monthly Balance',on_click=set_form_step,args=['Jump',12],type=monthly_balance_type)
 
+
 ### Replace Wizard Form Body with this ###
 def wizard_form_body():
     ###### Step 1: Age ######
     if st.session_state['current_step'] == 1:            
         st.markdown('\n')
         st.markdown('\n')
-        age = st.number_input('Type your age', value = age, on_change = update_age)
+     #    age = st.number_input('Type your age', value = age, on_change = update_age)
+        st.number_input('Type your age', key = '_age', value = st.session_state['age'], on_change = update_age)
      
     ###### Step 2: Income ######
     if st.session_state['current_step'] == 2:
         st.markdown('\n')
         st.markdown('\n')
-        income = st.number_input('Type your monthly income', value = income, on_change = update_income)        
+        st.number_input('Type your monthly income', key = '_income', value = st.session_state.income, on_change = update_income)        
 
     ###### Step 3: Month ######            
     if st.session_state['current_step'] == 3:
         st.markdown('\n')
         st.markdown('\n')
         months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-        month_options = st.selectbox('Month of credit solicitude:', options = months , index = 0, on_change = update_month)
-        if month_options == 'January':
-            month = '1'
-        elif month_options == 'February':
-            month = '2'
-        elif month_options == 'March':
-            month = '3'
-        elif month_options == 'April':
-            month = '4'
-        elif month_options == 'May':
-            month = '5'
-        elif month_options == 'June':
-            month = '6'
-        elif month_options == 'July':
-            month = '7'
-        elif month_options == 'August':
-            month = '8'
-        elif month_options == 'September':
-            month = '9'
-        elif month_options == 'October':
-            month = '10'
-        elif month_options == 'November':
-            month = '11'
-        elif month_options == 'December':
-            month = '12'  
+        st.selectbox('Month of credit solicitude:', key = 'numeric_month', options = months , index = 0, on_change = update_month)
 
     ###### Step 4: Bank Accounts ######
     if st.session_state['current_step'] == 4:
         st.markdown('\n')
         st.markdown('\n')                           
-        num_bank_accounts = st.number_input('Type the number of distinct bank accounts that you have:')     
+        st.number_input('Type the number of distinct bank accounts that you have:', key = '_bank_accounts', value = st.session_state.num_bank_accounts, on_change = update_bank_accounts)     
 
     ###### Step 5: Credit Cards ######
     if st.session_state['current_step'] == 5:
         st.markdown('\n')
         st.markdown('\n')                                
-        num_credit_card = st.number_input('Type the number of distinct credit cards that you have:')                
+        st.number_input('Type the number of distinct credit cards that you have:', key = '_credit_card', value = st.session_state.num_credit_card, on_change = update_credit_card)                
 
     ###### Step 6: Interest Rate ######
     if st.session_state['current_step'] == 6:
         st.markdown('\n')
         st.markdown('\n')                          
-        last_interest_rate = st.number_input('Type the last interest rate that you paid:')                             
+        st.number_input('Type the last interest rate that you paid:', key = '_last_interest_rate', value = st.session_state.last_interest_rate, on_change = update_last_interest_rate)                             
 
     ###### Step 7: Number of loans ######
     if st.session_state['current_step'] == 7:
         st.markdown('\n')
         st.markdown('\n')                          
-        Num_of_Loan = st.number_input('Type how many loans have you had in the past:')                
+        st.number_input('Type how many loans have you had in the past:', key = '_Num_of_Loan', value = st.session_state.Num_of_Loan, on_change = update_Num_of_Loan)                
 
     ###### Step 8: Maximum month delay ######
     if st.session_state['current_step'] == 8:
         st.markdown('\n')
         st.markdown('\n')                             
-        month_delay = st.number_input('Type the maximum month delay you have had from payment due date:')                
+        st.number_input('Type the maximum month delay you have had from payment due date:', key = '_month_delay', value = st.session_state.month_delay, on_change = update_month_delay)                
 
     ###### Step 9: Maximum payments delay ######
     if st.session_state['current_step'] == 9:
         st.markdown('\n')
         st.markdown('\n')                            
-        payments_delay = st.number_input('Type the maximum payments delay you have had:')                
+        st.number_input('Type the maximum payments delay you have had:', key = '_payments_delay', value = st.session_state.payments_delay, on_change = update_payments_delay)                
      
     ###### Step 10: Credit Mix ######
     if st.session_state['current_step'] == 10:
         st.markdown('\n')
         st.markdown('\n') 
         credit_mix_options = ['Good','Standard','Bad']
-        credit_mix_selection = st.selectbox('What is your credit mix status?',options=credit_mix_options,index=0)
-        if credit_mix_selection == 'Good':
-          credit_mix = '0'
-        elif credit_mix_selection == 'Standard':
-          credit_mix = '1'
-        elif credit_mix_selection == 'Bad':
-          credit_mix = '2'           
+        st.selectbox('What is your credit mix status?',options=credit_mix_options,index=0, key = '_credit_mix', on_change = update_credit_mix)      
 
-    ###### Step 11: Maximum payments delay ######
+    ###### Step 11: Credit History Age ######
     if st.session_state['current_step'] == 11:
         st.markdown('\n')
         st.markdown('\n')                               
-        Credit_History_Age = st.number_input('Type the number of years you have had a credit history:')     
+        st.number_input('Type the number of years you have had a credit history:', key = '_Credit_History_Age', value = st.session_state.Credit_History_Age, on_change = update_Credit_History_Age)     
 
     ###### Step 12: Monthly Balance ######
     if st.session_state['current_step'] == 11:
         st.markdown('\n')
         st.markdown('\n')                            
-        Monthly_Balance = st.number_input('Type your last monthly balance:')     
+        Monthly_Balance = st.number_input('Type your last monthly balance:', key = '_Monthly_Balance', value = st.session_state.Monthly_Balance, on_change = update_Monthly_Balance)  
             
     st.markdown('---')
 
@@ -355,7 +367,7 @@ def wizard_form_body():
         form_footer_cols[1].button('Back',on_click=set_form_step,args=['Back'],disabled=disable_back_button)
         form_footer_cols[2].button('Next',on_click=set_form_step,args=['Next', ],disabled=disable_next_button)
     
-        file_ready = False if st.session_state['current_step'] == 12 is not None else True
+        file_ready = False if st.session_state['current_step'] == 12 != None else True
         load_file = form_footer_cols[3].button('📤 Submit',disabled=file_ready)
          
 #     if load_file:
